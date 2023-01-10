e6ff3734046336b3d3d1a387ffe3782855f4a557
Curbelman <111905398+Curbelman@users.noreply.github.com>
2023-01-10 16:09:11 +0100

    completes iteration 5, done

--- src/App.js
+++ src/App.js
@@ -14,11 +14,48 @@ function App() {
     setContacts([...contacts, randomContact[0]]);
   };
 
+  const alphabeticalSort = () => {
+    const alphaSortedContacts = [
+      ...contacts.sort((a, b) => {
+        const contactA = a.name.toUpperCase();
+        const contactB = b.name.toLocaleUpperCase();
+        if (contactA < contactB) {
+          return -1;
+        }
+        if (contactA > contactB){
+          return 1;
+        }
+        return 0;
+      }),
+    ];
+    setContacts(alphaSortedContacts);
+  };
+
+  const popularitySort = () => {
+    const popSortedContacts = [
+      ...contacts.sort((a, b) => {
+        return b.popularity - a.popularity;
+      }),
+    ];
+    setContacts(popSortedContacts);
+  };
+
+  const deleteContact = (index) => {
+    const removedContact = [...contacts];
+    removedContact.splice(index, 1);
+
+    setContacts(removedContact);
+};
+
 return (
   <div className="App">
   <h1>IronContacts</h1>
 
 <button onClick={() => addNewRandomContact()}>Add Random Contact to list</button>
+<div>
+  <button onClick={() => alphabeticalSort()}>Sort by Name</button>
+  <button onClick={() => popularitySort()}>Sort by Popularity</button>
+</div>
 
     <table>
     <thead>
@@ -28,6 +65,7 @@ return (
         <th>Popularity</th>
         <th>Won Oscar</th>
         <th>Won Emmy</th>
+        <th>Delete</th>
       </tr>
     </thead>
     <tbody>
@@ -41,6 +79,7 @@ return (
           <td>{eachContact.popularity}</td>
           {eachContact.wonOscar ? <td>🏆</td> : <td></td>}
           {eachContact.wonEmmy ? <td>🏆</td> : <td></td>}
+          <td><button onClick={() => deleteContact(i)}>Delete</button></td>
         </tr>
       );
     })}
