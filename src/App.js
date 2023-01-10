d4d415922cf68d829a249276e8cb2b35b0d49e0b
Curbelman <111905398+Curbelman@users.noreply.github.com>
2023-01-10 15:15:30 +0100

    completes iteration 3

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import contactsList from "./contacts.json";
 import { useState } from "react";
 
 function App() {
-  const [contacts] = useState(contactsList.slice(0, 5));
+  const [contacts, setContacts] = useState(contactsList.slice(0, 5));
+  const otherContacts = contactsList.slice(5,contactsList.length);
+
+  const addNewRandomContact = () => {
+    let randomIndex = Math.floor(Math.random()*otherContacts.length);
+
+    let randomContact = otherContacts.splice(randomIndex,1);
+
+    setContacts([...contacts, randomContact[0]]);
+  };
 
 return (
   <div className="App">
+  <h1>IronContacts</h1>
+
+<button onClick={() => addNewRandomContact()}>Add Random Contact to list</button>
 
     <table>
     <thead>
@@ -14,6 +26,8 @@ return (
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
       </tr>
     </thead>
     <tbody>
@@ -25,6 +39,8 @@ return (
           </td>
           <td>{eachContact.name}</td>
           <td>{eachContact.popularity}</td>
+          {eachContact.wonOscar ? <td>🏆</td> : <td></td>}
+          {eachContact.wonEmmy ? <td>🏆</td> : <td></td>}
         </tr>
       );
     })}
